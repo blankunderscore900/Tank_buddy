88bff8355ac5ef6b68aa5e54f402dd8032040c4b
blankunderscore900 <55696064+blankunderscore900@users.noreply.github.com>
2019-10-07 10:58:39 -0300

    Update 0.1
    uploading to github

---
+++ Tank Buddy/Assets/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    public Transform CameraHolder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = 0;
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f);
+
+        rb.AddForce(movement * speed);
+
+        CameraHolder.transform.position = gameObject.transform.position;
+    }
+}
