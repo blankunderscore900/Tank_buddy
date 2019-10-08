78f07665df1265f7b5cb23e35b29b7138115db3d
blankunderscore900 <55696064+blankunderscore900@users.noreply.github.com>
2019-10-08 12:08:26 -0300

    added fire to the game
    adding fire to the game

--- Tank Buddy/Assets/Shooter_Scrpit.cs
+++ Tank Buddy/Assets/Shooter_Scrpit.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class Shooter_Scrpit : MonoBehaviour
 {
 
-    public GameObject bullet;
+    GameObject gun;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gun = Resources.Load("bullet") as GameObject; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(gun) as GameObject;
+            bullet.transform.position = transform.position + Camera.main.transform.right * 1;
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
+            rb.velocity = Camera.main.transform.right * 40;
+        }
     }
+
 }
